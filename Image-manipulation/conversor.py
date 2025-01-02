cc0692148c8d072e4dd4390fb8403c64ccf3b848
gabriel <gabrielsgonc.dev@gmail.com>
2025-01-01 22:30:51 -0300

    feat: image manipulation with opencv

---
+++ Image-manipulation/conversor.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('chan.jpg')
+
+kernel = np.ones((11, 11), np.float32)/121
+dst = cv2.filter2D(img, -1, kernel)
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+dst = cv2.cvtColor(dst, cv2.COLORBGR2RGB)
+
+plt.subplot((121), plt.imshow(img), plt.title('Original'))
+plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(dst), plt.title('Averaging')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+laplacian = cv2.Laplacian(img, cv2.CV_64F)
+sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+
+plt.subplot(2, 2, 1), plt.imshow(img, cmap='gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 2), plt.imshow(laplacian, cmap='gray')
+plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 3), plt.imshow(sobelx, cmap='gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 4), plt.imshow(sobely, cmap='gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+plt.show()
