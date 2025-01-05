36e784ec2b4a1dd12f6a5dc979678a0e49b5aa20
gabriel <gabrielsgonc.dev@gmail.com>
2025-01-05 15:27:20 -0300

    feat: image converson

--- Image-manipulation/conversor.py
+++ Image-manipulation/conversor.py
@@ -19,6 +19,7 @@ plt.show()
 laplacian = cv2.Laplacian(img, cv2.CV_64F)
 sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
 sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+sobelxy = cv2.Sobel(img, cv2.CV_64F, 1, 1, ksize=3)
 
 plt.subplot(2, 2, 1), plt.imshow(img, cmap='gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
